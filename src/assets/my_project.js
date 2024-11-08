cfdd7afa32960aa1493a60e17eec70afdd87b9f4
Nivethan Eswaran <140292846+Nivethan01@users.noreply.github.com>
2024-11-08 15:05:29 +0530

    Updation

--- src/assets/my_project.js
+++ src/assets/my_project.js
@@ -2,7 +2,7 @@ import Project01 from './Project 01.png';
 import Project02 from './Project 02.png';
 import Project03 from './Project 03.png';
 import Project04 from './Project 04.jpg';
-
+import Project05 from './Project 05.jpg';
 const myproject = [
   {
     p_no: 1,
@@ -27,6 +27,13 @@ const myproject = [
     p_name: "Roxiler Product Manager",
     p_img: Project04,
     p_link:"https://roxiler-pro.vercel.app/"
+  },
+  {
+    p_no:5,
+    p_name:"Chatly",
+    p_img:Project05,
+    p_link:"https://github.com/Nivethan01/C-W-C.git",
+    p_status:"Currently working"
   }
 ];
 
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import './project.css';
+import React from "react";
+import "./project.css";
 import projects from "../../assets/my_project";
 
 const Project = () => {
   return (
-    <div id='projects' className="projects">
+    <div id="projects" className="projects">
       <div className="mywork-title">
         <h1>My Projects</h1>
       </div>
       <div className="mywork-container">
         {projects.map((project, index) => (
-          project.p_link ? (
-            <a key={index} href={project.p_link} target="_blank" rel="noopener noreferrer">
+          <div key={index} className="project-item">
+            {project.p_link ? (
+              <a
+                href={project.p_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={project.p_img} alt={project.p_name} />
+              </a>
+            ) : (
               <img src={project.p_img} alt={project.p_name} />
-            </a>
-          ) : (
-            <img key={index} src={project.p_img} alt={project.p_name} />
-          )
+            )}
+            {project.p_status && (
+              <h5 className="project-status">
+                {project.p_status}
+              </h5>
+            )}
+          </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Project;
